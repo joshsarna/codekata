ce006f5633eb157c99d76ab981faf2a9bfd5b722
joshsarna <joshsarna@gmail.com>
2018-06-17 14:46:30 -0700

    write a program that recognizes the smallest spread and prints it

--- katacode_program.rb
+++ katacode_program.rb
@@ -1,3 +1,28 @@
-smallest_spread_day = 0
+# Create array with data
+input = File.open('./weather.dat', File::RDONLY){|f| f.read }
+array = input.lines.map{|l| l.split.map(&:to_f) }
 
-array.each do |day|
+# Organize array
+array = array.flatten
+array.delete(0.0)
+
+# Loop through array, looking for smallest spread
+index = 1
+smallest_spread_day = 0
+smallest_spread = 1000
+
+30.times do
+  spread = array[index + 1] - array[index + 2]
+  if spread < smallest_spread
+    smallest_spread = spread
+    smallest_spread_day = array[index]
+  end
+    index += 13
+    if array[index] > 1000
+      index += 1
+    end
+end
+
+# Print results
+puts "The smallest spread is #{smallest_spread}"
+puts "It occurs on day #{smallest_spread_day.to_i}" 
